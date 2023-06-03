3540bfbbb1dd34b957b81def55918ef2814ee104
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-03 12:21:55 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -31,7 +31,7 @@ async def auto_filter(bot: Bot, message: types.Message, text=True):
 @Bot.on_callback_query(filters.regex(r"^lang"))
 async def language_check(bot, query):
     _, userid, language = query.data.split("#")
-    if int(userid) not in [query.from_user.id, 0]:
+    if int(userid) in [query.from_user.id, 0]:
         return await query.answer(ALRT_TXT.format(query.from_user.first_name), show_alert=True)
     if language == "unknown":
         return await query.answer("↓ Channel နဲ့ Video Quality ရွေးချယ်ပါ။ ↓", show_alert=True)
