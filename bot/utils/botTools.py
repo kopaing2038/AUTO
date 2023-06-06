6a1ce88db9a0fce30c2b0812aa319c38aef5939f
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-06 14:40:12 +0630

    Update botTools.py

--- bot/utils/botTools.py
+++ bot/utils/botTools.py
@@ -137,6 +137,7 @@ async def format_buttons(files: list, channel: bool):
                 types.InlineKeyboardButton(
                     text=f" {file['file_name']} [{get_size(file['file_size'])}] ",
                     url=f'{(await parse_link(file["chat_id"], file["message_id"]))}',
+                    small=True,  # Display text in smallest font style
                 ),
             ]
             for file in files
@@ -147,16 +148,12 @@ async def format_buttons(files: list, channel: bool):
                 types.InlineKeyboardButton(
                     text=f"{file['file_name']}  [{get_size(file['file_size'])}] ",
                     callback_data=f"file {file['_id']}",
+                    small=True,  # Display text in smallest font style
                 ),
             ]
             for file in files
         ]
-    for row in btn:
-        for button in row:
-            button.text = f"<font face='Arial'>{button.text}</font>"
-
     return btn
-
 
 
 
