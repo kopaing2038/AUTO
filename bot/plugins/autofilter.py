7005359962353e2d9bd199ef715ba54dffdd5948
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-09 01:21:55 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -166,13 +166,14 @@ async def ch1_give_filter(bot: Bot, message: types.Message):
         files_a, offset, total_results_a = await a_filter.get_search_results(
             search.lower(), offset=0, filter=True, photo=settings['PHOTO_FILTER']
         )
-        files_b, offset, total_results_b = await b_filter.get_search_results(
-            search.lower(), offset=0, filter=True, photo=settings['PHOTO_FILTER']
-        )
+        if not files_a:
+            files_b, offset, total_results_b = await b_filter.get_search_results(
+                search.lower(), offset=0, filter=True, photo=settings['PHOTO_FILTER']
+            )
         # files = files_a + files_b  # Combine the files from both filters
         # total_results = total_results_a + total_results_b  # Combine the total results from both filters
-        if not files_b:
-            return
+            if not files_b:
+                return
     else:
         return
 
