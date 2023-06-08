e1cfbfab56a0612a6b5b480ea2e7d6137caf18bf
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-09 01:45:05 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -121,7 +121,7 @@ async def select_language(bot, query):
     if len(data_parts) < 3:
         return await query.answer("Invalid data format.", show_alert=True)
     req = query.from_user.id if query.from_user else 0
-    if (query.from_user.id or 0) not in [int(data_parts[2]), 0]:
+    if int(data_parts) not in [query.from_user.id, 0]:
         return await query.answer("This is not for you", show_alert=True)
     _, search = data_parts
 
