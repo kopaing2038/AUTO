0419f7a76a9b944ebcc6853b71da35fcfc91e419
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-09 16:38:26 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -805,8 +805,8 @@ async def ch3_give_filter(bot: Bot, message: types.Message):
         cap = f"𝗤𝘂𝗲𝗿𝘆   :{search}\n𝗧𝗼𝘁𝗮𝗹    : {total_results}\n𝗥𝗲𝗾𝘂𝗲𝘀𝘁 : {message.from_user.mention} \n\n</b><a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>\n<a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗙𝗜𝗟𝗘 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>"
     cap2 = f"𝗤𝘂𝗲𝗿𝘆   : {search}\n𝗧𝗼𝘁𝗮𝗹    : {total_results}\n𝗥𝗲𝗾𝘂𝗲𝘀𝘁 : {message.from_user.mention} \n\n</b><a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>\n<a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗙𝗜𝗟𝗘 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>"	
     ADS = [
-        {"photo": "https://graph.org/file/00644e75f1d747f4b132c.jpg", "caption": cap2},
-        {"photo": "https://graph.org/file/14b989e4cb562882f28c3.jpg", "caption": cap2},
+        {"photo": "https://graph.org/file/c40a9f62fdca19702e93c.jpg", "caption": cap2},
+        {"photo": "https://graph.org/file/c40a9f62fdca19702e93c.jpg", "caption": cap2},
     ]
     btn = btn_a + btn_b + btn_c
     if imdb and imdb.get("poster") and settings["IMDB_POSTER"]:
