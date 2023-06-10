06dfce9c722417a052d4ae4d3977ef21d45746f1
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-10 22:06:41 +0630

    Update speedtest.py

--- bot/plugins/kopaing/speedtest.py
+++ bot/plugins/kopaing/speedtest.py
@@ -9,7 +9,11 @@ SUDOERS = filters.user()
 async def testspeed(m):
     try:
         m = await m.edit("Running SpeedTest")
-        result = speedtest_cli.speedtest()
+        speedtester = speedtest_cli.Speedtest()
+        speedtester.get_best_server()
+        speedtester.download()
+        speedtester.upload()
+        result = speedtester.results.dict()
     except Exception as e:
         return await m.edit(str(e))
     return result
@@ -18,8 +22,8 @@ async def testspeed(m):
 async def speedtest_function(bot: Bot, message):
     m = await message.reply_text("Running Speed test")
     result = await testspeed(m)
-    download_speed = result.download / 1000000
-    upload_speed = result.upload / 1000000
+    download_speed = result['download'] / 1000000
+    upload_speed = result['upload'] / 1000000
     output = f"""**Speedtest Results**
 
 <u>**Speed:**</u>
@@ -28,20 +32,18 @@ async def speedtest_function(bot: Bot, message):
 **__Upload Speed:__** {upload_speed:.2f} Mbps
     
 <u>**Client:**</u>
-**__ISP:__** {result.client['isp']}
-**__Country:__** {result.client['country']}
+**__ISP:__** {result['client']['isp']}
+**__Country:__** {result['client']['country']}
 
 <u>**Server:**</u>
-**__Name:__** {result.server['name']}
-**__Country:__** {result.server['country']}, {result.server['cc']}
-**__Sponsor:__** {result.server['sponsor']}
-**__Latency:__** {result.server['latency']}  
-**__Ping:__** {result.server['ping']}"""
+**__Name:__** {result['server']['name']}
+**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Latency:__** {result['server']['latency']}  
+**__Ping:__** {result['ping']}"""
 
     msg = await bot.send_message(
         chat_id=message.chat.id,
         text=output
     )
     await m.delete()
-
-    
