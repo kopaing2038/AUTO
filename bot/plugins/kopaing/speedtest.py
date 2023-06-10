d262e0ccc3932b3454da5ca119a335daa819d389
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-10 21:45:46 +0630

    Update speedtest.py

--- bot/plugins/kopaing/speedtest.py
+++ bot/plugins/kopaing/speedtest.py
@@ -3,38 +3,25 @@ import speedtest
 from pyrogram import filters, Client
 from bot import Bot
 from pyrogram.types import Message, InlineKeyboardButton
-#from speedtest_cli import Speedtest
+import speedtest_cli 
 
 
 SUDOERS = filters.user()
 
 
-def testspeed(m):
+async def testspeed(m):
     try:
-        test = speedtest.Speedtest()
-        test.get_best_server()
-        m = m.edit("Running Download SpeedTest")
-        test.download()
-        m = m.edit("Running Upload SpeedTest")
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
-        m = m.edit("Sharing SpeedTest Results")
+        result = await asyncio.to_thread(speedtest_cli.speedtest)
+        return result
     except Exception as e:
-        return m.edit(e)
-    return result
+        return str(e)
 
 @Bot.on_message(filters.command(["speedtest"]) & ~filters.channel)
 async def speedtest_function(bot: Bot, message):
     m = await message.reply_text("Running Speed test")
-    loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m)
-    speedtester = speedtest.Speedtest()
-    speedtester.get_best_server()
-    download_speed = result['download'] if isinstance(result['download'], float) else result['download'].get('speed', '-')
-    upload_speed = result['upload'] if isinstance(result['upload'], float) else result['upload'].get('speed', '-')
-    download_speed = round(speedtester.download() / 1000000, 2)
-    upload_speed = round(speedtester.upload() / 1000000, 2)
+    result = await testspeed(m)
+    download_speed = round(result.download / 1000000, 2)
+    upload_speed = round(result.upload / 1000000, 2)
     output = f"""**Speedtest Results**
 
 <u>**Speed:**</u>
@@ -43,21 +30,19 @@ async def speedtest_function(bot: Bot, message):
 **__Upload Speed:__** {upload_speed}  Mbps
     
 <u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+**__ISP:__** {result.client['isp']}
+**__Country:__** {result.client['country']}
 
 <u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
-**__Ping:__** {result['ping']}"""
+**__Name:__** {result.server['name']}
+**__Country:__** {result.server['country']}, {result.server['cc']}
+**__Sponsor:__** {result.server['sponsor']}
+**__Latency:__** {result.server['latency']}  
+**__Ping:__** {result.ping}"""
 
     msg = await bot.send_photo(
         chat_id=message.chat.id, 
-        photo=result["share"], 
+        photo=result.share(),
         caption=output
     )
     await m.delete()
-
-
