8a6a8bd6f0daf2fc42f50ea8745b3eaba8f847f1
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-06-16 16:36:03 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -69,3 +69,10 @@ class Bot(Client):
 
 
 bot = Bot(Config.BOT_NAME)  # type: ignore
+
+@bot.on_message(filters.command("restart") & filters.user(Config.ADMINS))
+async def stop_button(bot, message):
+    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)
+    await asyncio.sleep(3)
+    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
