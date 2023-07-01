7edc2d25d5d1f5e98a08b8f07f6100068b9c2330
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-01 12:11:13 +0630

    Update botTools.py

--- bot/utils/botTools.py
+++ bot/utils/botTools.py
@@ -12,7 +12,7 @@ from .logger import LOGGER
 
 log = LOGGER(__name__)
 
-db = Database(Config.DATABASE_URI, Config.SESSION_NAME)
+db = (Config.DATABASE_URI, Config.SESSION_NAME)
 CONFIGURABLE = {
     "IMDB": {"help": "Enable or disable IMDB status", "name": "Imdb Info"},
     "CHANNEL": {"help": "Redirect to Channel / Send File", "name": "Channel"},
