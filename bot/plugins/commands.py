213e2148aa9052e7abf6690a4a0db7989577145b
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-03 10:02:39 +0630

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1150,9 +1150,9 @@ async def deleteindex(bot, message):
         }
     )
     if result.deleted_count:
-        await message.reply_text("File is successfully deleted {chat_id} from the database.")
+        await message.reply_text(f"File is successfully deleted {chat_id} from the database.")
     else:
-        await message.reply_text("File not found in{chat_id}the database.")
+        await message.reply_text(f"File not found in{chat_id}the database.")
 
 
 @Bot.on_callback_query(filters.regex(r'^autofilter_delete1'))
