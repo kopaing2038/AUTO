de4662847cfbfaa316141bbff7510b2679d52b91
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-03 10:31:51 +0630

    Update autofilter.py

--- bot/database/autofilter.py
+++ bot/database/autofilter.py
@@ -140,8 +140,8 @@ class BaseFiltersDb(MongoDb):
     async def get_distinct_chat_ids(self):
         return await self.col.distinct("chat_id")
 
-    async def delete_one(self, channel_id):
-        result = await self.col.delete_one({'chat_id': channel_id})
+    async def delete_many(self, channel_id):
+        result = await self.col.delete_many({'chat_id': channel_id})
         return result
 
 class FiltersDb(BaseFiltersDb):
