271034edac2bf8935b354c0cfeca99ebf46722ce
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-07 17:08:34 +0630

    Create mongoDb.py

---
+++ bot/clone_plugins/database/mongoDb.py
@@ -0,0 +1,22 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import AgnosticDatabase, AgnosticCollection
+
+from ..config import Config
+
+
+DATABASE_URL = Config.DATABASE_URL
+SESSION_NAME2 = Config.SESSION_NAME2
+
+class MongoDb:
+    
+    def __init__(self):
+        self._client = AsyncIOMotorClient(DATABASE_URL)
+        self.db : AgnosticDatabase = self._client[SESSION_NAME2]
+
+    def get_collection(self, name: str) -> AgnosticCollection:
+        return self.db[name]
+    
+
+    
+
+
