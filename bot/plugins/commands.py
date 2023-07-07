97f00a242357c161be7e5d6afe8262e0561585d5
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-07 10:17:01 +0630

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1288,17 +1288,9 @@ async def set_cap2_command(client, message):
     if len(message.command) < 3:
         await message.reply_text("Please provide a group ID and a caption to set for CAP2.")
         return
-
-    group_id_str = message.command[1]
+    
+    group_id = message.command[1]
     caption = " ".join(message.command[2:])
-
-    try:
-        group_id = int(group_id_str)
-    except ValueError:
-        await message.reply_text("Invalid group ID. Please provide a valid integer.")
-        return
-
-    settings = await configDB.get_settings('SETTINGS_', group_id)
-    settings['CAP2'] = caption
-    await configDB.update_settings(group_id, settings)
-    await message.reply_text("CAP2 updated successfully.")
+    
+    await configDB.update_group_settings(group_id, {'CAP2': caption})
+    await message.reply_text("CAP2 updated successfully for the group.")
