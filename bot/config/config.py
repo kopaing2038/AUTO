aa896f0f899ebc1a1419e5dcb7fb767c9cb0bbab
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-07 12:36:34 +0630

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -132,20 +132,10 @@ class Config:
 ────── • ◆ • ──────""")
 
 
-
-    ADS = [
-        {"photo": "https://graph.org/file/57c7be369ccd72eff94ee.jpg", "caption": f"""Caption 
-for 
-
-AD 1"""},
-        {"photo": "https://graph.org/file/7fb891f2249f1400c1fee.jpg", "caption": f"Caption 
-
-for AD 2"""},
-        {"photo": "https://graph.org/file/708102b70a54d649676d6.jpg", "caption": f"Caption for 
-
-AD 
-
-3"""},
+    ADS: List[dict] = [
+        {"photo": "https://graph.org/file/57c7be369ccd72eff94ee.jpg", "caption": "Caption for AD 1"},
+        {"photo": "https://graph.org/file/7fb891f2249f1400c1fee.jpg", "caption": "Caption for AD 2"},
+        {"photo": "https://graph.org/file/708102b70a54d649676d6.jpg", "caption": "Caption for AD 3"},
     ]
 
     CUSTOM_FILE_CAPTION2 = get_config(
