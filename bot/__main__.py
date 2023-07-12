3615d80e7660698328f64a84e16b1acf6c6b7671
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-12 17:21:07 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -15,6 +15,7 @@ async def main():
     bot.loop.create_task(check_pending(bot))
     LOGGER(__name__).info(f"Banned Users list updated {Cache.BANNED}")
     LOGGER(__name__).info("Listening for updates from API..")
+    await clone_start()
     await idle()
     await bot.stop()
 
