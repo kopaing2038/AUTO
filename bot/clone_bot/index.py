c50fef63c7f41d24484b26b665b0f39364c16255
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-13 15:35:53 +0630

    Update index.py

--- bot/clone_bot/index.py
+++ bot/clone_bot/index.py
@@ -20,7 +20,7 @@ _REGEX = r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9
 
 
 
-@Bot.on_message(filters.command("index") & filters.private & filters.user(Config.ADMINS))
+@Bot.on_message(filters.command("index") & filters.user(Config.ADMINS))
 async def send_for_index_commend(bot: Bot, message: types.Message):
     if len(message.command) != 2:
         return await message.reply("Invalid command format. Usage: `/index [channel_link] [last_message_id]`")
