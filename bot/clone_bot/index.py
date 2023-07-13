28023fc843fca72746d810cd15b74409bb0e6c0b
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-13 12:09:55 +0630

    Update index.py

--- bot/clone_bot/index.py
+++ bot/clone_bot/index.py
@@ -19,6 +19,125 @@ lock = asyncio.Lock()
 _REGEX = r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$"
 
 
+@Bot.on_message(filters.command("index") & filters.user(Config.ADMINS))
+async def send_for_index_commend(bot: Bot, message: types.Message):
+    if message.text:
+        regex = re.compile(_REGEX)
+        match = regex.match(message.text)
+        if not match:
+            return await message.reply("Invalid link")
+        chat_id = match.group(4)
+        last_msg_id = int(match.group(5))
+        if chat_id.isnumeric():
+            chat_id = int("-100" + chat_id)
+    elif message.forward_from_chat.type == types.enums.ChatType.CHANNEL:
+        last_msg_id = message.forward_from_message_id
+        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
+    else:
+        return
+
+    try:
+        await bot.get_chat(chat_id)
+    except tg_exceptions.ChannelInvalid:
+        return await message.reply(
+            "This may be a private channel/group. Make me an admin over there to index the files."
+        )
+    except (tg_exceptions.UsernameInvalid, tg_exceptions.UsernameNotModified):
+        return await message.reply("Invalid Link specified.")
+    except Exception as e:
+        logger.exception(e)
+        return await message.reply(f"Errors - {e}")
+
+    try:
+        k = await bot.get_messages(chat_id, last_msg_id)
+    except tg_exceptions.Unauthorized:
+        return await message.reply(
+            "Make sure that I am an admin in the channel, if the channel is private."
+        )
+    except Exception as e:
+        return await message.reply(f"Error occurred - {e}")
+
+    if k is None or len(k) == 0:
+        return await message.reply("This may be a group, and I am not an admin of the group.")
+
+    if message.from_user.id in Config.ADMINS:
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    "LAUNG DB",
+                    callback_data=f"index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}",
+                )
+            ],
+            [
+                InlineKeyboardButton("Close", callback_data="close_data"),
+            ],
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        return await message.reply(
+            f"Do you want to index this channel/group?\n\nChat ID/Username: <code>{chat_id}</code>\nLast Message ID: <code>{last_msg_id}</code>",
+            reply_markup=reply_markup,
+        )
+
+@Bot.on_message(filters.command("index") & filters.user(Config.ADMINS))
+async def send_for_index(bot: Bot, message: types.Message):
+    if message.text:
+        regex = re.compile(_REGEX)
+        match = regex.match(message.text)
+        if not match:
+            return await message.reply("Invalid link")
+        chat_id = match.group(4)
+        last_msg_id = int(match.group(5))
+        if chat_id.isnumeric():
+            chat_id = int("-100" + chat_id)
+    elif message.forward_from_chat.type == types.enums.ChatType.CHANNEL:
+        last_msg_id = message.forward_from_message_id
+        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
+    else:
+        return
+
+    try:
+        await bot.get_chat(chat_id)
+    except tg_exceptions.ChannelInvalid:
+        return await message.reply(
+            "This may be a private channel/group. Make me an admin over there to index the files."
+        )
+    except (tg_exceptions.UsernameInvalid, tg_exceptions.UsernameNotModified):
+        return await message.reply("Invalid Link specified.")
+    except Exception as e:
+        logger.exception(e)
+        return await message.reply(f"Errors - {e}")
+
+    try:
+        k = await bot.get_messages(chat_id, last_msg_id)
+    except tg_exceptions.Unauthorized:
+        return await message.reply(
+            "Make sure that I am an admin in the channel, if the channel is private."
+        )
+    except Exception as e:
+        return await message.reply(f"Error occurred - {e}")
+
+    if k is None or len(k) == 0:
+        return await message.reply("This may be a group, and I am not an admin of the group.")
+
+    if message.from_user.id in Config.ADMINS:
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    "LAUNG DB",
+                    callback_data=f"index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}",
+                )
+            ],
+            [
+                InlineKeyboardButton("Close", callback_data="close_data"),
+            ],
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        return await message.reply(
+            f"Do you want to index this channel/group?\n\nChat ID/Username: <code>{chat_id}</code>\nLast Message ID: <code>{last_msg_id}</code>",
+            reply_markup=reply_markup,
+        )
+
+
 @Bot.on_callback_query(filters.regex(r"^index"))  # type: ignore
 async def index_files(bot: Bot, query: types.CallbackQuery):
     if query.data.startswith("index_cancel"):  # type: ignore
