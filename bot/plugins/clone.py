24a93469f49cbf31f86f1f6e22e8957b75eb89cd
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-13 08:59:03 +0630

    Update clone.py

--- bot/plugins/clone.py
+++ bot/plugins/clone.py
@@ -32,9 +32,10 @@ async def savefiles(bot_id):
     mycol = mydb[str(bot_id)]
 
     try:
-        mycol.insert_one({'bot_id': bot_id})  # Insert a single document with 'bot_id' field
+        mycol.insert_one({bot_id: bot_id})  # Insert a single document with 'bot_id' field
     except Exception as e:
         print(f"Error while saving files: {e}")
+
 
 
 #@Client.on_message((filters.regex(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}')) & filters.private)
