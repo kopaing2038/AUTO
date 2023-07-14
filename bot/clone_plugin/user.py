499bc442f38667ad8818ed6ba7fde3903027fde0
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-14 12:26:12 +0630

    Create user.py

---
+++ bot/clone_plugin/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+from pyrogram import Client
+
+from . import API_HASH, APP_ID, LOGGER
+
+
+class User(Client):
+    def __init__(self):
+        super().__init__(
+            "userbot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            workers=20
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        return (self, usr_bot_me.id)
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
