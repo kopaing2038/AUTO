111f79f6944ef0be57cab0fdc276cb587dfd647c
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-14 15:24:17 +0630

    Update clone.py

--- bot/plugins/clone.py
+++ bot/plugins/clone.py
@@ -112,7 +112,7 @@ async def clone_v2_accept(client, message):
     await message.reply_text("Admin accept waiting", reply_markup=accept_button)
 
 
-@Bot.on_callback_query(filters.regex(r"^accept_"))
+@Client.on_callback_query(filters.regex(r"^accept_"))
 async def clone_v2(client, callback_query):
     try:
         user_id, bot_id = callback_query.data.split("_")[1:]
