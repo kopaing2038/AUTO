6f2f19c2227d9d1b81152b37d101cc69e9570a43
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-14 12:32:23 +0630

    Update bot.py

--- bot/clone_plugin/bot.py
+++ bot/clone_plugin/bot.py
@@ -1,1 +1,16 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @SpEcHIDe
+
+from pyrogram import Client, enums, __version__
+
+#from . import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
+
+from bot.clone_plugin.user import User
+
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+
+    
