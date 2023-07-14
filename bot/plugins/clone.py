eb4d2ef380b83b8de82f3f68df1d7747fbb1f7f8
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-14 14:22:20 +0630

    Update clone.py

--- bot/plugins/clone.py
+++ bot/plugins/clone.py
@@ -139,7 +139,7 @@ async def clone_v2(client, message):
             clonedme.B_NAME = bot.first_name
 
             if bot_ids:
-                a_filter = FiltersDb(bot_id, clonedme.U_NAME)
+                await a_filter(bot_id, clonedme.U_NAME)
                 await savefiles(clonedme.U_NAME)
                 #await add_bot(user_id, bot_id)
             
