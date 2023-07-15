325ffb8d64caa7f951560d64bcff41fb514ef008
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-15 12:32:23 +0630

    Update speedtest.py

--- bot/plugins/kopaing/speedtest.py
+++ bot/plugins/kopaing/speedtest.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import filters, Client
 from bot import Bot
-from pyrogram.types import Message, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton
 from speedtest_cli import Speedtest
 
 SUDOERS = filters.user()
@@ -22,16 +22,27 @@ async def testspeed(m):
 async def speedtest_function(bot: Bot, message):
     m = await message.reply_text("Running Speed test")
     result = await testspeed(m)
-    
     if isinstance(result, dict):
         download_speed = result.get('download', 0) / 1000000
         upload_speed = result.get('upload', 0) / 1000000
         client_country = result.get('client', {}).get('country', '')
+        server_name = result.get('server', {}).get('name', '')
+        server_country = result.get('server', {}).get('country', '')
+        server_cc = result.get('server', {}).get('cc', '')
+        server_sponsor = result.get('server', {}).get('sponsor', '')
+        server_latency = result.get('server', {}).get('latency', '')
+        server_ping = result.get('ping', '')
     else:
         download_speed = 0
         upload_speed = 0
         client_country = ''
-    
+        server_name = ''
+        server_country = ''
+        server_cc = ''
+        server_sponsor = ''
+        server_latency = ''
+        server_ping = ''
+
     output = f"""**Speedtest Results**
 
 <u>**Speed:**</u>
@@ -44,11 +55,11 @@ async def speedtest_function(bot: Bot, message):
 **__Country:__** {client_country}
 
 <u>**Server:**</u>
-**__Name:__** {result.get('server', {}).get('name', '')}
-**__Country:__** {result.get('server', {}).get('country', '')}, {result.get('server', {}).get('cc', '')}
-**__Sponsor:__** {result.get('server', {}).get('sponsor', '')}
-**__Latency:__** {result.get('server', {}).get('latency', '')}  
-**__Ping:__** {result.get('ping', '')}"""
+**__Name:__** {server_name}
+**__Country:__** {server_country}, {server_cc}
+**__Sponsor:__** {server_sponsor}
+**__Latency:__** {server_latency}  
+**__Ping:__** {server_ping}"""
 
     msg = await bot.send_message(
         chat_id=message.chat.id,
