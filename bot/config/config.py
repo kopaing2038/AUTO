fe1b0ada009427f36c605ad16ad8c81f5d7cdc20
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-15 09:51:56 +0630

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -202,7 +202,9 @@ REQUEST BY : {user_link}
     CH_POST = False
     TEXT_LINK = False
     SONG = False
-    
+    SPELLING = True
+    PHOTO_CAP = False
+
     GROUP_CAPTIONS = {} 
     MUSIC_CHANNEL = int(get_config("MUSIC_CHANNEL" , "-1001897618013")) 
     USE_CAPTION_FILTER = get_config("USE_CAPTION_FILTER", True, True)  # type: ignore
