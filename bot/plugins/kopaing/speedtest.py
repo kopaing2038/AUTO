b6a912b845f5e37c8741420dd07d348ca5131941
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2023-07-15 13:11:39 +0630

    Update speedtest.py

--- bot/plugins/kopaing/speedtest.py
+++ bot/plugins/kopaing/speedtest.py
@@ -93,44 +93,37 @@ async def start(bot, update):
 async def speedtest(_, message):
     speed = await message.reply_text("<i>Initializing Speedtest...</i>")
     test = Speedtest()
+    #test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = result['share']
+    string_speed = f'''
+➲ <b><i>SPEEDTEST INFO</i></b>
+┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+┠ <b>Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
+┠ <b>Ping:</b> <code>{result['ping']} ms</code>
+┠ <b>Time:</b> <code>{result['timestamp']}</code>
+┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+
+
+
+➲ <b><i>CLIENT DETAILS</i></b>
+┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
+┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
+┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+┠ <b>Country:</b> <code>{result['client']['country']}</code>
+┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
+┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
     try:
-        test.get_best_server()
-        test.download()
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
-        path = result['share']
-        string_speed = f'''
-        ➲ <b><i>SPEEDTEST INFO</i></b>
-        ┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
-        ┠ <b>Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
-        ┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-        ┠ <b>Time:</b> <code>{result['timestamp']}</code>
-        ┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
-        ┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
-
-        ➲ <b><i>SPEEDTEST SERVER</i></b>
-        ┠ <b>Name:</b> <code>{result['server']['name']}</code>
-        ┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-        ┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-        ┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-        ┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-        ┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
-
-        ➲ <b><i>CLIENT DETAILS</i></b>
-        ┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-        ┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-        ┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-        ┠ <b>Country:</b> <code>{result['client']['country']}</code>
-        ┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
-        ┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
-        '''
         await message.reply_photo(string_speed, photo=path)
+        await speed.delete()
     except Exception as e:
         logger.error(str(e))
-        await speed.edit_text("Failed to perform Speedtest. Please try again later.")
-
-    await speed.delete()
+        await speed.edit_text(string_speed)
 
 
 def speed_convert(size):
@@ -142,3 +135,4 @@ def speed_convert(size):
         size /= power
         zero += 1
     return f"{round(size, 2)} {units[zero]}"
+
